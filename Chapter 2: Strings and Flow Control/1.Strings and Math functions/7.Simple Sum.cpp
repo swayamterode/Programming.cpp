80c8ceceda5612027ab45f0f61d926243b8d91a2
swayamterode <swayam.programmer@gmail.com>
2023-06-20 02:09:49 +0530

    Simple Sum

---
+++ Chapter 2: Strings and Flow Control/1.Strings and Math functions/7.Simple Sum.cpp
@@ -0,0 +1,31 @@
+/**
+ * @file 7.Simple Sum.cpp
+ * @author Swayam Terode
+ * @brief
+ * @version 0.1
+ * @date 2023-06-20
+ *
+ * @copyright Copyright (c) 2023
+Problem Description
+Given three integers A, B, and C, your task is to print the sum of these three integers.
+
+Example 1:
+
+Input: A = 1, B = 2, C = 3
+Output: 6
+
+Example 2:
+
+Input: A = 5, B = 3, C = 2
+Output: 10
+ */
+
+class Solution
+{
+public:
+    int sumOfThree(int A, int B, int C)
+    {
+        int sum = A + B + C;
+        return sum;
+    }
+};
