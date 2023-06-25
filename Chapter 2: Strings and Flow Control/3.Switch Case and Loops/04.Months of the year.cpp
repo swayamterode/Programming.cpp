4c0cac803c7f6a80a140c2a6595e71c3ef621326
swayamterode <swayam.programmer@gmail.com>
2023-06-25 13:01:36 +0530

    Code: Months of the year

---
+++ Chapter 2: Strings and Flow Control/3.Switch Case and Loops/04.Months of the year.cpp
@@ -0,0 +1,8 @@
+class Solution
+{
+public:
+    vector<string> MonthsOfTheYear()
+    {
+        return {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
+    }
+};
