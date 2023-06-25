c2946ffe5c0a075426552fb2f1172fd4ef23a806
swayamterode <swayam.programmer@gmail.com>
2023-06-25 12:05:15 +0530

    Code: Sum of first N natural numbers

---
+++ Chapter 2: Strings and Flow Control/3.Switch Case and Loops/03.Sum of first N natural numbers.cpp
@@ -0,0 +1,13 @@
+class Solution
+{
+public:
+    int sumOfNaturalNumbers(int n)
+    {
+        int sum = 0;
+        for (int i = 1; i <= n; i++)
+        {
+            sum += i;
+        }
+        return sum;
+    }
+};
